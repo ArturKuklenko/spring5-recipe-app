12f47d792d4aae4a0adc22a572324bf06010ce0c
Artur Kuklenko <kuklenko2020@gmail.com>
2026-05-09 17:58:38 +0300

    Add RecipeService interface.

---
+++ src/main/java/guru/springframework/service/RecipeServiceImpl.java
@@ -0,0 +1,29 @@
+package guru.springframework.service;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+    private RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    public List<Recipe> getList() {
+        try {
+            List<Recipe> recipeList = (List<Recipe>) this.recipeRepository.findAll();
+            return recipeList;
+        } catch (Exception e) {
+
+        }
+        return new ArrayList<Recipe>();
+    }
+
+}
